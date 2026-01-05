f760ab6cbc23b69407ef987c2face2fef20a1831
lsoledispa0614-pixel <lsoledispa0614@utm.edu.ec>
2026-01-05 13:34:44 +0100

    Configurar DB con variables para Supabase

--- src/db.js
+++ src/db.js
@@ -1,11 +1,14 @@
+import "dotenv/config";
 import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  user: "invent_user",
-  host: "localhost",
-  database: "inventario",
-  password: "12345",
-  port: 5432,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false },
 });
+
 
